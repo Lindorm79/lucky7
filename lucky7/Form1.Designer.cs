30f4918f3b2956a5791ab10e31451197f3c0df79
Salvatore <Salvatore@DESKTOP-OMLU52J>
2023-10-27 00:01:33 +0100

    Add project files.

---
+++ lucky7/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace lucky7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelTítulo = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTítulo
+            // 
+            this.labelTítulo.AutoSize = true;
+            this.labelTítulo.Font = new System.Drawing.Font("Times New Roman", 26F);
+            this.labelTítulo.ForeColor = System.Drawing.Color.Green;
+            this.labelTítulo.Location = new System.Drawing.Point(293, 19);
+            this.labelTítulo.Name = "labelTítulo";
+            this.labelTítulo.Size = new System.Drawing.Size(133, 40);
+            this.labelTítulo.TabIndex = 0;
+            this.labelTítulo.Text = "Lucky 7";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(272, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 42);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "SPIN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(362, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 42);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "EXIT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Arial", 24.25F);
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(232, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 80);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "0";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Arial", 24.25F);
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(321, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 80);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "0";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Arial", 24.25F);
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(410, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 80);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "0";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.Control;
+            this.label6.Font = new System.Drawing.Font("Arial", 12.25F);
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(285, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(141, 19);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Saldo: 10 créditos";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(214, 234);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(311, 167);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_2);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(196, 9);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(83, 69);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(432, 9);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(83, 69);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 19;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelTítulo);
+            this.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.Name = "Form1";
+            this.Text = "Lucky7";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTítulo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ lucky7/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace lucky7
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        private int saldo = 10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Saldo()
+        {
+            label6.Text = "Saldo: " + saldo + " créditos";
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (saldo >= 1)
+            {
+                pictureBox1.Visible = false;
+                int result1 = random.Next(10);
+                int result2 = random.Next(10);
+                int result3 = random.Next(10);
+                label3.Text = result1.ToString();
+                label4.Text = result2.ToString();
+                label5.Text = result3.ToString();
+                // Verifica se três "7s" iguais foram sorteados
+                if (result1 == 7 && result2 == 7 && result3 == 7)
+                {
+                    saldo += 5; // Jackpot - ganha 5 créditos
+                    MessageBox.Show("Jackpot! Você ganhou 5 créditos!");
+                    pictureBox1.Visible = true;
+                }
+                else if (result1 == 7 && result2 == 7 || result2 == 7 && result3 == 7)
+                {
+                    saldo += 2; // Dois "7s" iguais - ganha 2 créditos
+                    MessageBox.Show("Você ganhou 2 créditos!");
+                    pictureBox1.Visible = true;
+                }
+                else if (result1 == 7 || result2 == 7 || result3 == 7)
+                {
+                    saldo += 1; // Um "7" - ganha 1 crédito
+                    MessageBox.Show("Você ganhou 1 crédito!");
+                    pictureBox1.Visible = true;
+                }
+                else
+                {
+                    saldo -= 1; // Desconta 1 crédito por rodada
+                    MessageBox.Show("Você perdeu 1 crédito. Tente novamente.");
+                }
+                Saldo(); // Atualiza o rótulo do saldo na interface
+            }
+            else
+            {
+                MessageBox.Show("Saldo insuficiente. Recarregue para continuar.");
+                Application.Exit();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult sair = MessageBox.Show("Tem a certeza que quer sair?", "Confirmação de Saída", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            // Verifica a escolha do usuário
+            if (sair == DialogResult.Yes)
+            {
+                // O usuário escolheu sair, então o programa é fechado
+                Application.Exit();
+            }
+        }
+        private void label5_Click(object sender, EventArgs e)
+        {
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+        private void label6_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
